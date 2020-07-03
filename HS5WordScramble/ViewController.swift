7e3b2608117b147f0959a41981428faa8f3a124d
Aaron Shackelford <burntcreamlatte@icloud.com>
2020-07-03 13:07:06 -0700

    Initial Commit

---
+++ HS5WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HS5WordScramble
+//
+//  Created by Aaron Shackelford on 7/3/20.
+//  Copyright © 2020 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
